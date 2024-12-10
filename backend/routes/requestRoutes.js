4f8227e45892279f9bbca1bd010fab6cff0ce546
Design And Creation <romain.doyen97@gmail.com>
2024-12-10 11:46:14 +0400

    Added additional routes for retrieving id, creating, updating and deleting requests

--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const { getAllRequests, createRequest } = require("../controllers/requestController");
+const { getAllRequests, createRequest, getRequestById, updateRequest, deleteRequest } = require("../controllers/requestController");
 
 const router = express.Router();
 
 router.get("/", getAllRequests);
 
+router.get("/:id", getRequestById);
+
 router.post("/", createRequest);
+
+router.put("/:id", updateRequest);
+
+router.delete("/:id", deleteRequest);
 
 module.exports = router;
